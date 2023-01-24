ee3343a6807d2da325812748ac1262fabef4337e
Mourad RL CADI <mouradface1998@gmail.com>
2023-01-25 00:30:31 +0100

    save

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,23 @@
-use std::{process::Command, str::from_utf8};
 use std::env::args;
+use std::{net::TcpStream, io::Read};
+use ssh2::Session;
 
-fn exe(cmd: &str) -> String {
-    let err = format!("Failed to execute command <{}>", cmd);
-
-    let output = if cfg!(target_os = "windows") {
-        Command::new("cmd")
-                .args(["/C", cmd])
-                .output()
-                .expect(&err)
-    } else {
-        Command::new("sh")
-                .arg("-c")
-                .arg(cmd)
-                .output()
-                .expect(&err)
-    };
-
-    String::from(from_utf8(&output.stdout).unwrap())
-}
+// fn exe(cmd: &str) -> String {
+//     let err = format!("Failed to execute command <{}>", cmd);
+//     let output = if cfg!(target_os = "windows") {
+//         Command::new("cmd")
+//                 .args(["/C", cmd])
+//                 .output()
+//                 .expect(&err)
+//     } else {
+//         Command::new("sh")
+//                 .arg("-c")
+//                 .arg(cmd)
+//                 .output()
+//                 .expect(&err)
+//     };
+//     String::from(from_utf8(&output.stdout).unwrap())
+// }
 
 fn main() {
     let _args: Vec<String> = args().collect();
@@ -31,7 +30,6 @@ fn main() {
     let branch = format!("git pull origin {}", _args[1]);
 
     let cmds = [
-        "ssh root@example.com",
         "cd /home/www/example.com",
         &branch,
         "npm run build",
@@ -42,9 +40,34 @@ fn main() {
         "pm2 restart node .output/server/index.mjs",
     ];
 
-    for cmd in cmds {
-        let result = exe(cmd);
+    // Connect to the local SSH server
+    let tcp = TcpStream::connect("site.azbuka-novostroek.com:22").unwrap();
 
-        println!("{:?}", result);
+    let mut sess = Session::new().unwrap();
+
+    sess.set_tcp_stream(tcp);
+
+    sess.handshake().unwrap();
+    
+    // Try to authenticate with the first identity in the agent.
+    sess.userauth_agent("root").unwrap();
+    
+    // Make sure we succeeded
+    assert!(sess.authenticated());
+
+    let mut channel = sess.channel_session().unwrap();
+
+    for cmd in cmds {
+        channel.exec(cmd).unwrap();
+
+        let mut s = String::new();
+    
+        channel.read_to_string(&mut s).unwrap();
+    
+        println!("{}", s);
     }
+
+    channel.wait_close().unwrap();
+
+    println!("{}", channel.exit_status().unwrap());
 }
